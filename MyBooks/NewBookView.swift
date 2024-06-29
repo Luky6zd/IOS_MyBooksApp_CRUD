47784cc26e12f2418db8f8b13a074b3f02d65e1d
Lucrezia Odrljin <lucrezia.mestrov@gmail.com>
2024-06-29 14:11:15 +0200

    added create and read from SQLite DB

---
+++ MyBooks/NewBookView.swift
@@ -0,0 +1,18 @@
+//
+//  NewBookView.swift
+//  MyBooks
+//
+//  Created by Lucrezia Odrljin on 28.06.2024..
+//
+
+import SwiftUI
+
+struct NewBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewBookView()
+}
